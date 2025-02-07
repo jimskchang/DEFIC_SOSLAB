a7d146d0a29f2c7673fd464237cc178362c50bb3
Jim <jimskchang@gmail.com>
2025-02-07 21:27:14 +0800

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -9,11 +9,11 @@ UDP_HEADER_LEN = 8
 ICMP_HEADER_LEN = 8
 L3_PROC = ['ip', 'arp']
 L4_PROC = ['tcp', 'udp', 'icmp']
-host = '10.1.100.233'
+host = '192.168.23.200'
 
 
 # NOTE: Settings
-NIC = 'enp3s0'
+NIC = 'ens192'
 NICAddr = '/sys/class/net/%s/address' % NIC
 record_path = 'pkt_record.txt'
 mac = b'\x00\x50\x56\xb0\x10\xe9'
